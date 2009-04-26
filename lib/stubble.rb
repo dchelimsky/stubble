b0b4a75d3ff32545d04be736657d9b83a3ab6be2
David Chelimsky <dchelimsky@gmail.com>
2009-04-26 08:39:48 -0500

    cleaner load path extension

--- lib/stubble.rb
+++ lib/stubble.rb
@@ -1,5 +1,5 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+lib = File.expand_path(File.dirname(__FILE__))
+$:.unshift(lib) unless $:.include?(lib)
 
 module Stubble
   VERSION = '0.0.1'
