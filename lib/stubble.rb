64f578747847fe3af3a8d8a67d91625dfd8f9429
David Chelimsky <dchelimsky@gmail.com>
2009-05-03 10:48:47 -0500

    make the version 0.0.0 because rubygems isn't quite able to find prerelease versions yet

--- lib/stubble.rb
+++ lib/stubble.rb
@@ -2,7 +2,7 @@ lib = File.expand_path(File.dirname(__FILE__))
 $:.unshift(lib) unless $:.include?(lib)
 
 module Stubble
-  VERSION = '0.0.0.a'
+  VERSION = '0.0.0'
 
   def stubble(klass, options={:savable => true})
     instance = stub(
