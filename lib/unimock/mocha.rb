9ed6b67d42ad157d7850707d9dc42a24c5a71c1d
David Chelimsky <dchelimsky@gmail.com>
2009-05-10 14:21:59 -0700

    better mocha monkey patch

--- lib/unimock/mocha.rb
+++ lib/unimock/mocha.rb
@@ -2,21 +2,19 @@ require 'mocha'
 
 module Mocha
   class Expectation
-    alias_method :orig_returns, :returns
-    def returns(*values, &block)
-      @stubble_block = block
-      orig_returns(*values)
-    end
-    
     alias_method :orig_invoke, :invoke
     def invoke
-      @stubble_block ? @stubble_block.call(*@actual_parameters) : orig_invoke
+      @invocable_block ? @invocable_block.call(*@actual_parameters) : orig_invoke
     end
     
     alias_method :orig_match?, :match?
     def match?(actual_method_name, *actual_parameters)
       @actual_parameters = actual_parameters
       orig_match?(actual_method_name, *actual_parameters)
+    end
+    
+    def invokes(&block)
+      @invocable_block = block
     end
   end
 end
@@ -33,7 +31,7 @@ module Unimock
   end
   
   def stub_and_invoke(obj, method, &block)
-    obj.stubs(method).returns(&block)
+    obj.stubs(method).invokes(&block)
   end
 
   def reset
